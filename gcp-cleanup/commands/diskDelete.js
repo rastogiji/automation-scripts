56f710d32c2165f373f729eb8632582e25cbf9fe
Animesh Rastogi <animesh.rastogi54@gmail.com>
2022-06-28 19:29:45 +0530

    Deleting unattached disks

---
+++ gcp-cleanup/commands/diskDelete.js
@@ -0,0 +1,41 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const uuid = require("uuid");
+
+const diskDelete = async () => {
+  const { stdout, stderr } = await exec(
+    `gcloud compute disks list --format="json(name,lastAttachTimestamp,zone.basename())"`
+  );
+  const disks = JSON.parse(stdout);
+
+  if (disks.length > 0) {
+    diskCleanup(disks);
+  } else {
+    console.log("No Disks Detected");
+  }
+};
+
+const diskCleanup = async (disks) => {
+  disks.forEach(async (disk) => {
+    let pointer = 0;
+    if (disk.lastAttachTimestamp === undefined) {
+      pointer++;
+      console.log(`${disk.name} is Unattached. Snapshotting...`);
+      await exec(
+        `gcloud compute disks snapshot ${disk.name} \
+        --zone=${disk.zone} --snapshot-names=${disk.name}-snapshot-${uuid.v4()}`
+      );
+      console.log("Successfully created the snapshot");
+      console.log("Deleting...");
+      await exec(
+        `gcloud compute disks delete ${disk.name} --zone=${disk.zone} --quiet`
+      );
+      console.log("Disk Successfully Deleted");
+    }
+    if (pointer === 0) {
+      console.log("All Disks are Attached");
+    }
+  });
+};
+
+module.exports = diskDelete;
